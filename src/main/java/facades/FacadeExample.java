440ed5e986ea12291ca34ec9d6dc9cb9d3bd8863
AlbertSylv <cph-al324@cphbusiness.dk>
2020-09-15 12:10:37 +0200

    Entity

--- src/main/java/facades/FacadeExample.java
+++ src/main/java/facades/FacadeExample.java
@@ -1,6 +1,6 @@
 package facades;
 
-import entities.RenameMe;
+import entities.Members;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
