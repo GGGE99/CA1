90f8dd3507d312783a1717065fd729a339cdaef7
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2020-09-15 13:44:28 +0200

    rest

--- src/main/java/rest/GroupmembersResource.java
+++ src/main/java/rest/GroupmembersResource.java
@@ -2,8 +2,10 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import entities.Member;
 import utils.EMF_Creator;
 import facades.MemberFacade;
+import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,8 +15,8 @@ import javax.ws.rs.core.MediaType;
 @Path("/groupmembers")
 public class GroupmembersResource {
 
-//    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-//    private static final MemberFacade FACADE =  MemberFacade.getMemberFacade(EMF);
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final MemberFacade FACADE =  MemberFacade.getMemberFacade(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
             
     @GET
@@ -27,6 +29,7 @@ public class GroupmembersResource {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String getAllMembers() {
-        return "{GAY}";  //Done manually so no need for a DTO
+        List<Member> allMembers = FACADE.getAllMembers();
+        return GSON.toJson(allMembers);  
     }
 }
