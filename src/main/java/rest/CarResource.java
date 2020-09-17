324eceaa412fd7fc829b7df6d351f27f32b0de3b
Mads Højris Kristensen <cph-mk613@cphbusiness.dk>
2020-09-17 12:25:41 +0200

    added endpoints

--- src/main/java/rest/CarResource.java
+++ src/main/java/rest/CarResource.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
-/**
- *
- * @author marcg
- */
+import DTOs.CarDTO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import facades.CarFacade;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import utils.EMF_Creator;
+
+
+@Path("/cars")
 public class CarResource {
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final CarFacade FACADE = CarFacade.getCarFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+
+    @Path("/all")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAllMembers() {
+        List<CarDTO> allMembers = FACADE.getAllCars();
+        return GSON.toJson(allMembers);
+    }
+    
+    
+    @Path("/populate")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String populate() {
+        FACADE.populateDB();
+        return "4 Members has been added";
+    }
 }
+
